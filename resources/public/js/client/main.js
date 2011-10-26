5c997416bd232d9564c41934d0c960ec57124f31
Trent Strong <trent@ichange.com>
2011-10-25 23:13:28 -0700

    Added metronome for timing beats.  Cleaned up prototype interface.

--- resources/public/js/client/main.js
+++ resources/public/js/client/main.js
@@ -8,6 +8,19 @@ dc.canvas;
 dc.stage;
 dc.io;
 
+window.requestAnimationFrame = (function(){
+    return (
+        window.requestAnimationFrame       ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame    ||
+        window.oRequestAnimationFrame      ||
+        window.msRequestAnimationFrame     ||
+        function(callback, element){
+            window.setTimeout(callback, 1000 / 60);
+        }
+    );
+})();
+
 dc.init = function () {
     soundManager.url = '/js/soundmanager/swf/';
     soundManager.flashVersion = 9; // optional: shiny features (default = 8)
@@ -66,9 +79,72 @@ dc.init = function () {
         }
     }
 
-    var sequenceView = new dc.SequenceView();
-    var sequenceDiv = sequenceView.render().el;
-    $('#sequencer-holder').append(sequenceDiv);
+    _.each(_.range(0, 4), function() {
+        var sequenceView = new dc.SequenceEditor();
+        var sequenceDiv = sequenceView.render().el;
+        $('#sequencer-holder').append(sequenceDiv);
+    });
+
+    dc.Metronome.start();
+    var metronome = new dc.Metronome(120);
+    metronome.listen(function(beat) { console.log(beat); });
+    
+}
+
+/**
+ *  Metronome 
+ *
+ *  Responsible for keeping track of the current beat.  Multiple instances can be created, all tracking different BPMs.
+ */
+
+
+/**
+ * Constructor
+ *
+ * Creates new metronome given the BPM.
+ */
+dc.Metronome = function(bpm) {
+    this.bpm = bpm;
+    this.beat = 1;
+    this.nextBeat = 0;
+    this.listeners = [];
+    dc.Metronome.instances.push(this);
+}
+
+/**
+ *  STATIC 
+ */
+dc.Metronome.instances = [];
+dc.Metronome.start = function() {
+    (function loop() {
+        var currentTimestamp = (+new Date()) / 1000;
+        for (var i=0; i < dc.Metronome.instances.length; i++) {
+            dc.Metronome.instances[i].tick(currentTimestamp);
+        }
+        window.requestAnimationFrame(loop);
+    })();
+}
+
+/**
+ *  PUBLIC 
+ */
+dc.Metronome.prototype = {
+    tick: function(currentTimestamp) {
+        if (currentTimestamp >= this.nextBeat) {
+            this.beat = ~~(((currentTimestamp / 60) * this.bpm) % 16)+1;
+            this.nextBeat = currentTimestamp + (60 / this.bpm);
+            for (var i=0; i < this.listeners.length; i++) {
+                this.listeners[i](this.beat);
+            }
+        }
+    },
+
+    /**
+     * Calls back on every beat 
+     */
+    listen: function(listener) {
+        this.listeners.push(listener);
+    }
 }
 
 dc.SequenceNote = Backbone.Model.extend({
@@ -96,10 +172,14 @@ dc.Sequence = Backbone.Collection.extend({
                 });
             });
         }
+    },
+
+    comparator: function(note) {
+        return chapter.get("beat");
     }
 })
 
-dc.SequenceView = Backbone.View.extend({
+dc.SequenceEditor = Backbone.View.extend({
 
     tagName: "div",
 
@@ -110,11 +190,16 @@ dc.SequenceView = Backbone.View.extend({
             this.collection = new dc.Sequence();
         }
         this.collection.each(this.addNote.bind(this));
+        $(this.el).addClass('clearfix');
     },
 
     addNote: function(note) {
         var view = new dc.SequenceNoteView({ model: note});
         $(this.el).append(view.render().el);
+    },
+
+    getSequence: function() {
+        return this.collection;
     }
 });
 
